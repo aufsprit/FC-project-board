36a137d6416fcdfff9910409fbc16d616840c276
brevity_ <dbtmdwls200@naver.com>
2022-11-15 23:48:44 +0900

    #38 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야 패스워드 인코더가 읽을 수 있음.
    이에 `{noop}`을 붙혀줌.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,14 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('aufsprit', 'asdf1234', 'aufsprit', 'aufsprit@mail.com', 'I am aufsprit.', now(), 'aufsprit', now(), 'aufsprit')
+    ('aufsprit', '{noop}asdf1234', 'aufsprit', 'aufsprit@mail.com', 'I am aufsprit.', now(), 'aufsprit', now(), 'aufsprit')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('aufsprit2', 'asdf1234', 'aufsprit2', 'aufsprit2@mail.com', 'I am aufsprit2.', now(), 'aufsprit2', now(), 'aufsprit2')
+    ('aufsprit2', '{noop}asdf1234', 'aufsprit2', 'aufsprit2@mail.com', 'I am aufsprit2.', now(), 'aufsprit2', now(), 'aufsprit2')
+; -- noop : no operation
+
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+    ('aufsprit3', '{noop}asdf1234', 'aufsprit3', 'aufsprit3@mail.com', 'I am aufsprit3.', now(), 'aufsprit3', now(), 'aufsprit3')
 ;
 
 -- 123 게시글
